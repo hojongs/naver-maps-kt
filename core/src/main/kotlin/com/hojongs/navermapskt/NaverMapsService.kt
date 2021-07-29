c8511e00daade33cec9bea1977b6e5744f3e6927
Jongho Jeon <hojong.jjh@gmail.com>
2021-07-30 01:11:15 +0900

    Add reverseGeocode method

--- core/src/main/kotlin/com/hojongs/navermapskt/NaverMapsService.kt
+++ core/src/main/kotlin/com/hojongs/navermapskt/NaverMapsService.kt
@@ -1,11 +1,17 @@
 package com.hojongs.navermapskt
 
+import com.hojongs.navermapskt.geocode.Geocode
 import com.hojongs.navermapskt.geocode.GeocodeRequest
 import com.hojongs.navermapskt.http.client.NaverHttpClient
+import com.hojongs.navermapskt.reversegc.ReverseGCRequest
+import com.hojongs.navermapskt.reversegc.ReverseGCResponse
 
 class NaverMapsService(
     private val naverHttpClient: NaverHttpClient,
 ) {
-    suspend fun geocode(geocodeRequest: GeocodeRequest) =
+    suspend fun geocode(geocodeRequest: GeocodeRequest): Geocode =
         naverHttpClient.geocode(geocodeRequest)
+
+    suspend fun reverseGeocode(reverseGCRequest: ReverseGCRequest): ReverseGCResponse =
+        naverHttpClient.reverseGeocode(reverseGCRequest)
 }
