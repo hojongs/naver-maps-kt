630c511ecfac613014e65b46a48037738cd4af0e
Jongho Jeon <hojong.jjh@gmail.com>
2021-07-29 18:47:31 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "com.hojongs.navermapskt"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
